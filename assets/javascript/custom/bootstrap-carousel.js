f21325c885e8f10bab805a72b1436862ef7b9972
Steven Gerritsen <steven.gerritsen@kadaster.nl>
2019-08-14 08:01:47 +0200

    test carousel

--- assets/javascript/custom/bootstrap-carousel.js
+++ assets/javascript/custom/bootstrap-carousel.js
@@ -13,3 +13,6 @@ $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
         next.children(':first-child').clone().appendTo($(this));
     }
 });
+$('.carousel').carousel({
+    interval: false
+});
